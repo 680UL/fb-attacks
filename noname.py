171d4e094935fc56fe695608295d79d5677ee0e4
Arkar Kyaw <47052658+arkarkyaw2138@users.noreply.github.com>
2019-12-01 10:58:02 +0630

    Python Script

---
+++ noname.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+os.system("clear")
+os.system("figlet FB-Attack")
+print
+print("Author   : MR.$!lent_680UL")
+print("github   : https://github.com/arkarkyaw2138")
+print("Facebook : https://www.facebook.com/arkar.kyaw.27")
+print
+ip = input(" Target ID: ")
+
+os.system("clear")
+os.system(" figlet FB-Attack Start")
+print ("Min May Sout Phote Ko Hack Par Lar 0% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 10%")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 20% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 35% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 50% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 65% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 75% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 85% ")
+time.sleep(5)
+print ("Min May Sout Phote Ko Hack Par Lar 100% ")
+print("Phin Khan Like Ngar Loe Ma That Lee Ko Facebook Hack Chin Tar Lar")
+os.system("figlet Phin Kan Ma Lar Thin Pay Mal")
